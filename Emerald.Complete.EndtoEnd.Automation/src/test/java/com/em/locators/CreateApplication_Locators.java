5f86ade6a3de9975882ba22a874d771310a3aec6
Administrator <Administrator@DESKTOP-OAC4TPN>
2020-12-03 18:10:44 +0500

    New Message

--- Emerald.Complete.EndtoEnd.Automation/src/test/java/com/em/locators/CreateApplication_Locators.java
+++ Emerald.Complete.EndtoEnd.Automation/src/test/java/com/em/locators/CreateApplication_Locators.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 import com.em.common.HelperMethods;
 
 public class CreateApplication_Locators {
-	
+
 	WebDriver driver;
 	HelperMethods HelpMethod = new HelperMethods();
 
@@ -18,25 +18,43 @@ public class CreateApplication_Locators {
 		PageFactory.initElements(driver, this);
 
 	}
-	
-	
-	
+
 	@FindBy(how = How.XPATH, using = "//div[@class='fab-wheel-saveOnly']")
 	WebElement SaveApplication_Button;
-	
 	@FindBy(how = How.XPATH, using = "//div[@class='Toastify__toast-body']/div/div/div")
 	WebElement CreateApplication_Alert;
+	@FindBy(how = How.XPATH, using = "//*[@id='toastAppErrors']")
+	WebElement AlertClosedWindow;
 	
-	
+	@FindBy(how = How.XPATH, using = "//*[@id='2']/div/div/h4/span")
+	WebElement CreateApplication_CompanyText;
+	@FindBy(how = How.ID, using = "companyName")
+	WebElement CompanyField;
+
 	public void clickOnSaveApplicationButton() {
 		HelperMethods.waitForElementToBeClickable(SaveApplication_Button);
 		SaveApplication_Button.click();
 	}
-	
+	public void clickOnErrorMessageAlert() {
+		HelperMethods.waitForElementToBeClickable(AlertClosedWindow);
+		AlertClosedWindow.click();
+	}
+
 	public String GetRequiredfields_errormessages() {
 		HelpMethod.waitForElementToBeVisible(CreateApplication_Alert);
 		return CreateApplication_Alert.getText();
 
+	}
+
+	public String GetCompanyText() {
+		HelpMethod.waitForElementToBeVisible(CreateApplication_CompanyText);
+		return CreateApplication_CompanyText.getText();
+
+	}
+	
+	public void typeCompanyName(String CompName) {
+		HelpMethod.waitForElementToBeVisible(CompanyField);
+		CompanyField.sendKeys(CompName);
 	}
 
 }
--- Emerald.Complete.EndtoEnd.Automation/src/test/java/com/em/testcases/CreateApplication_TestCases.java
+++ Emerald.Complete.EndtoEnd.Automation/src/test/java/com/em/testcases/CreateApplication_TestCases.java
@@ -3,7 +3,6 @@ package com.em.testcases;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import com.em.common.BrowsersFactory;
 import com.em.common.HelperMethods;
 import com.em.locators.CreateApplication_Locators;
@@ -28,11 +27,24 @@ public class CreateApplication_TestCases extends BrowsersFactory {
 		}
 	}
 	@Test(priority = 14)
-	public void ClickOn_Save_or_CreateApplication_Button() {
+	@Parameters({ "Requiredfields_errorMessagess"})
+	public void ClickOn_Save_or_CreateApplication_Button(String Requiredfields_errorMessagess) {
 		CreateApplicationPage = new CreateApplication_Locators(driver);
 		CreateApplicationPage.clickOnSaveApplicationButton();
-		String GetAll_errorMessage = CreateApplicationPage.GetRequiredfields_errormessages();
-		System.out.println(GetAll_errorMessage);
+		String Get_Message = CreateApplicationPage.GetRequiredfields_errormessages();
+		System.out.println("Required Error Message withoutApplication " +Get_Message);
+		CreateApplicationPage.clickOnErrorMessageAlert();
+		//Assert.assertEquals(GetAll_errorMessage, Requiredfields_errorMessagess);
+		
 	}
+	@Test(priority = 15)
+	@Parameters({"CommText"})
+	public void CheckCompany_sectionText_areAvailble(String CommText) {
+		CreateApplicationPage = new CreateApplication_Locators(driver);
+		String Get_Message = CreateApplicationPage.GetCompanyText();
+		Assert.assertEquals(Get_Message, CommText);
+		
+	}
+	
 
 }
